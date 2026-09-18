4866f334180dcce22beef254683b183e4ed0a023
Kaay <kaay_uni@outlook.com>
2026-09-18 21:23:58 +0800

    fix: nt6.2内核(Windows Server 2012、Windows 8)支持 (#1)

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, path::PathBuf};
 
 fn main() {
+    // thunk::thunk();
     let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
     let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
     slint_build::compile_with_config("ui/app.slint", config).expect("Slint build failed");
@@ -11,5 +12,13 @@ fn main() {
         // pub const LANG_CHINESE_SIMPLIFIED: u32 = 4u32;
         res.set_language(4u16);
         res.compile().expect("资源文件信息编译失败");
+
+        #[cfg(feature = "win8")]
+        {
+            cc::Build::new()
+                .file("compat/win8_compat.c")
+                .compile("win8_compat");
+            println!("cargo:rustc-link-arg=-Wl,--wrap=SystemParametersInfoForDpi");
+        }
     }
 }
